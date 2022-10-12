e9314aa0ef18cdb0a8e0a6f723fc2241bfbd722e
vflorin1995 <vflorin1995@gmail.com>
2022-10-12 15:21:19 +0300

    Built dist file

--- dist/main.js
+++ dist/main.js
@@ -7,6 +7,48 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 
 
+const list = document.querySelector('.items');
+const playerName = document.querySelector('.name');
+const playerScore = document.querySelector('.score');
+const submit = document.querySelector('.submit');
+const refresh = document.querySelector('.refresh');
+
+const apiKey = 'SVuoD7OpR0ZoVo2RZZhq';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores/`;
+
+refresh.addEventListener('click', async () => {
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data = data.result;
+      list.innerHTML = '';
+      data.forEach((element) => {
+        const listElement = document.createElement('ul');
+        listElement.classList = 'flex';
+        const name = document.createElement('li');
+        name.innerText = `${element.user}:`;
+        const score = document.createElement('li');
+        score.innerText = element.score;
+        listElement.append(name, score);
+        list.append(listElement);
+      });
+    });
+});
+
+submit.addEventListener('click', async () => {
+  const object = {
+    user: playerName.value,
+    score: playerScore.value,
+  };
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(object),
+  })
+    .then((response) => response.json());
+  playerName.value = '';
+  playerScore.value = '';
+});
 
 /***/ }),
 /* 1 */
